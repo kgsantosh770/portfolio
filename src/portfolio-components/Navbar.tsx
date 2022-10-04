a2613df863aab45b81b5d1dc057ef9ca3e01a8e7
Santosh <s.kunga.ganeshbabu@accenture.com>
2022-10-04 17:38:36 +0530

    mobile navbar animation

--- src/portfolio-components/Navbar.tsx
+++ src/portfolio-components/Navbar.tsx
@@ -1,12 +1,11 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { PersonalData } from "../portfolio-data"
 
 function Navbar() {
+    const [hamburgerActive, setHamburgerActive] = useState(false);
     useEffect(() => {
         const sections = document.querySelectorAll("section");
         window.onscroll = () => {
-            var current = "";
-
             sections.forEach((section) => {
                 const sectionTop = section.offsetTop;
                 if (window.scrollY >= sectionTop - 200) {
@@ -20,6 +19,15 @@ function Navbar() {
         };
     }, [])
 
+    useEffect(() => {
+        const html = document.documentElement;
+        if (window.innerWidth <= 991) {
+            hamburgerActive ?
+                html.style.overflow = "hidden" :
+                html.style.overflow = "auto";
+        }
+    }, [hamburgerActive])
+
     const setActiveMenu = (classname: string) => {
         classname = ".menu-item.menu-" + classname;
         const menuItems = document.querySelectorAll(".menu-item");
@@ -29,18 +37,23 @@ function Navbar() {
         const clickedElement = document.querySelector(classname);
         clickedElement?.classList.add("active");
     }
+
     return (
-        <nav>
+        <nav className={hamburgerActive ? "active" : ""}>
             <a href="#about" className="name-logo">
                 <span className="header-title">{PersonalData.name.logo}</span>
             </a>
+            <div className="hamburger" onClick={() => setHamburgerActive(prevState => !prevState)}>
+                <span className="line"></span>
+                <span className="line"></span>
+                <span className="line"></span>
+            </div>
             <ul className="menu">
-                <li><a href="#about" className="menu-item un  menu-about active">About</a></li>
-                <li><a href="#experience" className="menu-item un menu-experience">Experience</a></li>
-                <li><a href="#projects" className="menu-item un menu-projects">Projects</a></li>
-                {/* <li><a href="#resume" className="menu-item un menu-resume">Resume</a></li> */}
-                <li><a href="#contact" className="menu-item un menu-contact">Contact</a></li>
-                <li><a href={PersonalData.resumeLink} className="btn">Resume</a></li>
+                <li><a href="#about" className="menu-item un  menu-about active" onClick={() => setHamburgerActive(prevState => !prevState)}>About</a></li>
+                <li><a href="#experience" className="menu-item un menu-experience" onClick={() => setHamburgerActive(prevState => !prevState)}>Experience</a></li>
+                <li><a href="#projects" className="menu-item un menu-projects" onClick={() => setHamburgerActive(prevState => !prevState)}>Projects</a></li>
+                <li><a href="#contact" className="menu-item un menu-contact" onClick={() => setHamburgerActive(prevState => !prevState)}>Contact</a></li>
+                <li><a href={PersonalData.resumeLink} className="btn" onClick={() => setHamburgerActive(prevState => !prevState)}>Resume</a></li>
             </ul>
         </nav>
     )
